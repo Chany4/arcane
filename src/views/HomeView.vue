a0171f4e5e464c1d3374d4d1220c1aded2ff13ab
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-07-11 09:05:15 -0700

    KYS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,13 @@
 <template>
   <FullscreenImage/>
- <HelloWorld/>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
 import FullscreenImage from '../components/OnLoad.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
     FullscreenImage
   }
 }
